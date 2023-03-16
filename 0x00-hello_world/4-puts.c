459d8c07185a4e6a9f9191aa6663205bdfa2448b
Mutisomary <marymutso2016@gmail.com>
2023-03-16 12:15:29 -0700

    Puts.C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Puts function program
+ *
+ * Return: 0 (Succecc)
+ * /
+ int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+Return(0);
+}
