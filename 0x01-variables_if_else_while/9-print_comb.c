422a1040df5cf295959133f0cda8eb2a356d85f7
Mutisomary <marymutso2016@gmail.com>
2023-03-21 03:01:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Prints all posiible combinations of singel digit numbers.
+*
+* Return: 0 success.
+*/
+
+int main(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
