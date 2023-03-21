925af4cbd67357651aa77101faab14630796c89e
Mutisomary <marymutso2016@gmail.com>
2023-03-21 02:12:15 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Prints numbers of base 16 in lowercase.
+*
+* Return: 0.
+*/
+
+int main(void)
+{
+        int c = '0';
+	char l = 'a';
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
+
+
