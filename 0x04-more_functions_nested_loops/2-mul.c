b30af54866b1034e1891e554d7567d8245416297
Mutisomary <marymutso2016@gmail.com>
2023-03-23 23:03:25 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - Function that multiplies two integers.
+*
+* @a: Parameter to be checked.
+* @b: Parameter to be checked.
+* Return: 0.
+*/
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
