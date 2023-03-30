2fb5534cd7c8935f6830d41a5dffe121b4bd3f9e
Mutisomary <marymutso2016@gmail.com>
2023-03-30 02:07:33 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,7 +21,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; src[i] != '\0'; i++)
 		srclen++;
 
-	for (i = 0; i < n ; i++)
+	for (i = 0; src[i] && i < n ; i++)
 	{
 		dest[destlen + i] = src[i];
 	}
