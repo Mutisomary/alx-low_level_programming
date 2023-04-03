ee1cdadeb97f4fe8392cdf291d1621e5f2b7ab8b
Mutisomary <marymutso2016@gmail.com>
2023-04-03 02:47:07 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _memcpy - Function that copies memory area.
+* @dest: destination value.
+* @src: source.
+* @n: Function parameter.
+* Return: Dest.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
