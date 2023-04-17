11565d63c18da4b0074646bd7ede5d19898b5fcd
Mutisomary <marymutso2016@gmail.com>
2023-04-17 01:53:47 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog's information.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: Attributes of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *p, char *name, float age, char *owner);
+void print_dog(struct dog *p);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *p);
+
+#endif
+
