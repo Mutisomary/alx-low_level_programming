94de7bb6be98b2da9bfb014a1ef8ba281125c9a9
Mutisomary <marymutso2016@gmail.com>
2023-04-17 02:43:20 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - Function that prints a struct dog.
+* @d: structure of a dog.
+* Return: 0.
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
