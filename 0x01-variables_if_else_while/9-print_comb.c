b96374ea8b770a59e37d1da21c80ccb378643f37
Mutisomary <marymutso2016@gmail.com>
2023-04-25 00:16:02 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-/**
-* main - Prints all posiible combinations of singel digit numbers.
-*
-* Return: 0 success.
-*/
 
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i = '0';
+	int n;
 
-	for (i = '0'; i <= '9'; i++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
 }
+
