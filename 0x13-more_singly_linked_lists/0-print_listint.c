e01e5bb60730be5d444ee37f5739c8a42d8d29ff
Mutisomary <marymutso2016@gmail.com>
2023-04-30 22:41:36 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
-* print_listint - Prints all the elements of a list.
-* @h: linked list.
-* Return: Total number of nodes.
-*/
-
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t s = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		s++;
+		num++;
 		h = h->next;
 	}
-	return (s);
+
+	return (num);
 }
+
