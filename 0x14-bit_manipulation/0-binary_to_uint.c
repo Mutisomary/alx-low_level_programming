64f816e8501d811d56e49fdabf15e74410a767c3
Mutisomary <marymutso2016@gmail.com>
2023-05-16 10:38:16 -0700

    101-natural.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
+ * @b: pointer to the string containing the binary number
  *
  * Return: the converted number
  */
---
+++ 101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int limit = 1024;
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("The sum of the multiples of 3 or 5 below %d is: %d\n", limit, sum);
+
+	return (0);
+}
+
