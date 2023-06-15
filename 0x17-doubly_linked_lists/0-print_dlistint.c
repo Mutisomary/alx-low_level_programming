298e2771d1b1f3b4805d01bf9720f4e6fd0b516e
Mutisomary <marymutso2016@gmail.com>
2023-06-15 02:54:56 -0700

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,12 +12,6 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *current = h;
 	size_t count = 0;
 
-	if (current == NULL)
-	{
-		printf("The doubly linked list is empty.\n");
-		return (count);
-	}
-
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
