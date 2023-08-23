2173a9d25da011aec1a59cae52172ed538cd553d
Mutisomary <marymutso2016@gmail.com>
2023-08-23 12:18:11 -0700

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,14 +6,14 @@ def island_perimeter(grid):
     """Find the perimeter of an island"""
     length_row = len(grid)
     length_column = len(grid[0])
-    perimeter = 0;
-    connections = 0;
+    perimeter = 0
+    connections = 0
 
     for i in range(0, length_row):
         for j in range(0, length_column):
             if grid[i][j] == 1:
                 perimeter += 4
-                
+
                 if i != 0 and grid[i - 1][j] == 1:
                     connections += 1
 
