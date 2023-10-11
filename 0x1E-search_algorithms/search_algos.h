56dca83b068f76aa3a5c1d20b4f916a752d6c4ab
Mutisomary <marymutso2016@gmail.com>
2023-10-11 06:02:46 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,9 @@
-#ifndef SEARCH_ALOGS_H
-#define SEARCH_ALOGS_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 int linear_search(int *array, size_t size, int value);
