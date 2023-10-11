a88f7c23d783ed4c7faf99335c260839577279a1
Mutisomary <marymutso2016@gmail.com>
2023-10-11 06:03:19 -0700

    jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - search a value using  jump search alogarithm 
+ * @array: pointer to the first element of the array
+ * @size: number of elements
+ * @value: value to be searched
+ * Return: -1 or index of the index of the value
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = 0, i = 0;
+
+	if (array != NULL)
+	{
+		while (high < size && array[high] < value)
+		{
+			low = high;
+			high = high + sqrt(size);
+			printf("Value checked array[%lu] = [%d]\n",
+			       low, array[low]);
+		}
+		printf("Value found between indexes [%lu] and [%lu]\n",
+		       low, high);
+		for (i = low; i <= high; i++)
+		{
+			if (i < size)
+			{
+				printf("Value checked array[%lu] = [%d]\n",
+				       i, array[i]);
+				if (array[i] == value)
+					return (i);
+			}
+		}
+	}
+	return (-1);
+}
